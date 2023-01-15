c5bdaa92e07b6adf99b553a9714f61f13909ea92
Cristian-Urrea <cristian.urrea4@gmail.com>
2023-01-15 10:48:07 +0100

    First commit

---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,70 @@
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import { calendarApi } from "../api";
+import { convertToDateEvents } from "../helpers";
+import {
+  onAddNewEvent,
+  onDeleteEvent,
+  onLoadEvents,
+  onSetActiveEvent,
+  onUpdateEvent,
+} from "../store";
+
+export const useCalendarStore = () => {
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
+
+  const setActiveEvent = (calendarEvent) => {
+    dispatch(onSetActiveEvent(calendarEvent));
+  };
+
+  const startSavingEvent = async (calendarEvent) => {
+    try {
+      if (calendarEvent.id) {
+        // Actualizando
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent);
+        dispatch(onUpdateEvent({ ...calendarEvent, user }));
+      }
+      // Creando
+      const { data } = await calendarApi.post("/events", calendarEvent);
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
+    } catch (error) {
+      console.log(error);
+      Swal.fire("Error al guardar", error.response.data.msg, "error");
+    }
+  };
+
+  const startDeletingEvent = async () => {
+    try {
+      await calendarApi.delete(`/events/${activeEvent.id}`);
+
+      dispatch(onDeleteEvent());
+    } catch (error) {
+      console.log(error);
+      Swal.fire("Error al eliminar", error.response.data.msg, "error");
+    }
+  };
+
+  const startLoadingEvents = async () => {
+    try {
+      const { data } = await calendarApi.get("/events");
+      const events = convertToDateEvents(data.events);
+      dispatch(onLoadEvents(events));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    //* Propiedades
+    activeEvent,
+    events,
+    hasEventSelected: !!activeEvent,
+
+    //* Métodos
+    startDeletingEvent,
+    setActiveEvent,
+    startSavingEvent,
+    startLoadingEvents,
+  };
+};
